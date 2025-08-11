fb8be66c17784ea8d5280c1510cefc6ad38ab5d7
Choa0Kim <asjklcom@gmail.com>
2025-08-11 16:28:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ next_minute = (minute + 1) % 60  # 0~59 범위 유지
 url = f"https://jsonplaceholder.typicode.com/todos/{next_minute}"
 
 # API 호출
-res = requests.get(url)
-data = res.json()
+test = requests.get(url)
+data = test.json()
 
 # /tmp/test.json에 저장
 with open("/tmp/test.json", "w") as f:
     json.dump(data, f, indent=2)
+
